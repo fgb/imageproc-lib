d6c4ab1dfbf8e6c9a35a741d8c3bb3489faaec9e
Fernando L. Garcia Bermudez <fgb@eecs.berkeley.edu>
2013-02-19 13:09:10 -0800

    Get rid of unnecessary interrupts.h

--- interrupts.c
+++ interrupts.c
@@ -7,17 +7,13 @@
 #include "utils.h"
 
 
-/******************************************************************************
- * Interrupt handlers for useful traps
- *
- * Note: Adapted from Microchip's traps.c
- */
-
+// Interrupt handlers for useful traps (adapted from Microchip's traps.c)
 void __attribute__((__interrupt__, no_auto_psv)) _AddressError(void)
 {
-        INTCON1bits.ADDRERR = 0;        //Clear the trap flags
-        while(1) {
-            LED_2 = ~LED_2;
-            delay_ms(50);
-        };
+        INTCON1bits.ADDRERR = 0; //Clear the trap flags
+
+        while (1)
+        {
+            LED_2 = ~LED_2; delay_ms(50);
+        }
 }
